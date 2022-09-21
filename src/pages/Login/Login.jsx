4e06c254624f8169b4b677cc0c67e01641dc5b27
ThanhNha <thanhnha.k13@gmail.com>
2022-09-21 15:38:47 +0700

    page Login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="container">
+      <h2>Login</h2>
+      <hr />
+    </div>
+  );
 }
