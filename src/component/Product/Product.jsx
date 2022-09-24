0eda747be3127784caa27f69f6b243e8e851d5d0
ThanhNha <thanhnha.k13@gmail.com>
2022-09-24 17:40:51 +0700

    thêm tính năng render feature của component product và scss

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 
 export default function Product({ product }) {
+  const renderFeature = () => {
+    const value = product?.feature;
+    if (value === true) {
+      return <i className=" fa  fa-heart feature_like"></i>;
+    }
+    return <i className=" fa  fa-heart feature"></i>;
+  };
   return (
     <div className="card">
       <img src={product.image} alt="..." />
+      {renderFeature()}
       <div className="card-body">
         <h3>{product.name}</h3>
-        <p>{product.price}</p>
-        <button className="btn btn-dark">
-          Add to cart
-          <i class="fa fa-cart-plus"></i>
-        </button>
+        <p>{product.shortDescription}</p>
+        <div>
+          <button>Buy now</button>
+          <p>{product.price}</p>
+        </div>
       </div>
     </div>
   );
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,5 +1,92 @@
-import React from "react";
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
+import { useSearchParams } from "react-router-dom";
+import Product from "../../component/Product/Product";
+// let timeout = null;
 export default function Search() {
-  return <div>Search</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [arrProduct, setArrProduct] = useState([]);
+
+  const typingTimeoutRef = useRef(null);
+  const handleChange = (e) => {
+    if (typingTimeoutRef.current) {
+      clearTimeout(typingTimeoutRef.current);
+    }
+    typingTimeoutRef.current = setTimeout(() => {
+      const value = e.target.value;
+      console.log(value);
+      setSearchParams({
+        keyword: value,
+      });
+    }, 500);
+  };
+  const getProductByKeywordApi = async () => {
+    // call api (b2-> gọi apo sau lần đầu tiên load trang)
+    try {
+      if (searchParams.get("keyword") !== null) {
+        const result = await axios({
+          url: `https://shop.cyberlearn.vn/api/Product?keyword=${searchParams.get(
+            "keyword"
+          )}`,
+          method: "GET",
+        });
+
+        setArrProduct(result.data.content);
+        // b3: sau khi lấy api thành công về -> state thay đổi -> giao diện render lại (Kết thúc lần 1 )
+        // Lần 2 (Bước 6)
+        console.log(result.data.content);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  // useEffect(() => {
+  //   timeout = setTimeout(() => {
+  //     getProductByKeywordApi();
+  //   }, 300);
+  //   return () => {
+  //     if (timeout !== null) {
+  //       clearTimeout(timeout);
+  //     }
+  //   };
+  // }, [searchParams.get("keyword")]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  useEffect(() => {
+    if (typingTimeoutRef.current) {
+      clearTimeout(typingTimeoutRef.current);
+    }
+    typingTimeoutRef.current = setTimeout(() => {
+      getProductByKeywordApi();
+    }, 500);
+  }, [searchParams.get("keyword")]);
+  return (
+    <div className="container">
+      <form className="search " onSubmit={handleSubmit}>
+        <div className="form-group ">
+          <p>Search</p>
+          <input
+            placeholder="Product name"
+            name="search"
+            id="search"
+            className="form-control"
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Search</button>
+      </form>
+      <h3>Search Result</h3>
+      <div className="row">
+        {arrProduct?.map((prod, index) => {
+          return (
+            <div className="col-3" key={index}>
+              <Product product={prod} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
